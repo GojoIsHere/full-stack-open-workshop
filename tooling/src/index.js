5c75c15f7b7acb4601304ca0d048159ce8b7260f
GojoIsHere <sus.thanet0726@gmail.com>
2023-07-20 06:25:00 +0545

    removed all files and trying to add from start again

--- tooling/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Head1 from "./Hello";
-
-let container = document.getElementById("root");
-let root = createRoot(container);
-
-root.render(React.createElement(Head1));
