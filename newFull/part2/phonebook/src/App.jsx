e5dc01578f8887bc951a4f89ed394c51fdd162d6
GojoIsHere <sus.thanet0726@gmail.com>
2023-08-15 17:00:36 +0545

    refactored 2

--- newFull/part2/phonebook/src/App.jsx
+++ newFull/part2/phonebook/src/App.jsx
@@ -6,7 +6,23 @@ const App = () => {
     console.log(e.target.value);
     setNewName(e.target.value);
   };
-
+  const handleClick = (e) => {
+    e.preventDefault();
+    let a = {};
+    a.name = newName;
+    let exists;
+    person.map((val) => {
+      if (val.name === a.name) {
+        return (exists = true);
+      }
+    });
+    if (exists) {
+      alert(`${a.name} is already added to phonebook `);
+    } else setPerson(person.concat(a));
+  };
+  const Numbers = ({ names }) => {
+    return <div>{names}</div>;
+  };
   return (
     <div>
       <div>
