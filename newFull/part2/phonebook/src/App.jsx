8bfe016c2d98db6af581baa381a48a07b5ae3323
GojoIsHere <sus.thanet0726@gmail.com>
2023-08-17 05:37:21 +0545

    feat: starting to add filter options

--- newFull/part2/phonebook/src/App.jsx
+++ newFull/part2/phonebook/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 const App = () => {
   // hook states
   const [person, setPerson] = useState([
-    { name: "Arto Hellas", number: 9800000000 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -49,7 +52,6 @@ const App = () => {
   // function for filter
   const handleFilter = (e) => {
     setFileter(e.target.value);
-    person.name.toLowerCase().includes(filter.toLowerCase());
   };
 
   return (
@@ -59,6 +61,7 @@ const App = () => {
         <span>Filter showen with :</span>
         <input type="text" value={filter} onChange={handleFilter} />
       </div>
+      <h3>Add a new</h3>
       <div>
         <form>
           <span>Name</span>
